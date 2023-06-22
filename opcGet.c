0fb4453ef20b289c5b22a0f4354b86142beadc01
AmineCoder30 <99543252+AmineCoder30@users.noreply.github.com>
2023-06-22 16:51:29 +0100

    Add files via upload

---
+++ opcGet.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * opcodeGet - get the code must be excuted
+ * @opc: opcode
+ * Return: return a ptr
+ * amine mohamed
+ */
+void (*opcodeGet(char *opc))(stack_t **stk, unsigned int nmLn)
+{
+	instruction_t instruct[] = {
+		{"push", node_push},
+		{"pall", stack_pall},
+		{"pint", stack_pint},
+		{"pop", stack_pop},
+		{"swap", stack_swap},
+		{"add", stack_add},
+		{"nop", stack_nop},
+		{NULL, NULL}
+	};
+	int l;
+
+	for (l = 0; instruct[l].opcode; l++)
+	{
+		if (compare_str(instruct[l].opcode, opc) == 0)
+			break;
+	}
+
+	return (instruct[l].f);
+}
